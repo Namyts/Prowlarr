b9cf8e16302d6fdf2795fe596a98703026898664
Namyts <35004248+Namyts@users.noreply.github.com>
2026-09-07 12:07:08 +0100

    identical requests wait for the first to produce results. removed ttl reset on cache hit

--- src/NzbDrone.Core.Test/IndexerSearchTests/ReleaseSearchServiceFixture.cs
+++ src/NzbDrone.Core.Test/IndexerSearchTests/ReleaseSearchServiceFixture.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,6 +26,10 @@ namespace NzbDrone.Core.Test.IndexerSearchTests
             Mocker.GetMock<IIndexerFactory>()
                 .Setup(s => s.Enabled(It.IsAny<bool>()))
                 .Returns(new List<IIndexer> { _mockIndexer.Object });
+
+            Mocker.GetMock<IReleaseSearchCache>()
+                .Setup(s => s.GetOrSearch(It.IsAny<NewznabRequest>(), It.IsAny<List<int>>(), It.IsAny<bool>(), It.IsAny<Func<Task<NewznabResults>>>()))
+                .Returns<NewznabRequest, List<int>, bool, Func<Task<NewznabResults>>>((_, _, _, search) => search());
         }
 
         private List<SearchCriteriaBase> WatchForSearchCriteria()
